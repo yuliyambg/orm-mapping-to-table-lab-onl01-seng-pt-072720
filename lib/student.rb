80214d2e7eb1b63d4722f9099e24389a28e3291e
Yuliya Marinova <yuliyambg@gmail.com>
2020-09-27 20:12:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -36,7 +36,7 @@ class Student
     VALUES (?, ?)
     SQL
     DB[:conn].execute(sql,name,grade)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 
 end
