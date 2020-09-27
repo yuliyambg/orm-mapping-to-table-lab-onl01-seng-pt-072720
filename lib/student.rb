3c3f979b360d1bf6980f625a17cbc4fcbdad0a20
Yuliya Marinova <yuliyambg@gmail.com>
2020-09-27 19:55:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,7 +35,7 @@ class Student
     INSERT INTO students (name, grade)
     VALUES (?,?)
     SQL
-    DB[:conn].execute(sql,self.name,self.grade)
+    DB[:conn].execute(sql,name,grade)
   end
 
 end
